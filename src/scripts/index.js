1b35a37dd0e8c03d64c7efb90bd433dc2bd6c298
fedeatanasoff <fede.atanasoff@gmail.com>
2019-03-31 19:40:06 -0300

    bootstrap, jquery y popper via npm

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,7 @@
 import { saludar } from "./utils";
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
 import "../styles/index.scss";
 
 const saludo = saludar("hola a todos!");
